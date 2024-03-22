267d02a5e21a92a24a7a263803556b56f9a67962
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2024-03-22 10:51:43 +0900

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,3 +1,15 @@
+function downloadFile(filename, content) {
+    var blob = new Blob([content], {type: "text/plain"});
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
 function createFileButton(fileId) {
     var button = document.createElement('button');
 
