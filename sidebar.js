3084ef6695bd3ccebded36e1d3d72d9d4fa6f2b1
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2024-03-22 10:48:43 +0900

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,21 +1,3 @@
-function toggleSidebar() {
-    var sidebarWidth = $('#sidebar').width();
-
-    // 사이드바와 콘텐츠의 'active' 클래스 토글
-    $('#sidebar, #content').toggleClass('active');
-
-    // 사이드바의 너비에 따라 콘텐츠와 버튼의 위치 조절
-    if ($('#sidebar').hasClass('active')) {
-        $('#content').css('margin-left', sidebarWidth + 'px');
-        $('#sidebarCollapse').css('left', sidebarWidth + 37 + 'px');
-        $('#toggleIcon').removeClass('fa-arrow-right').addClass('fa-arrow-left');
-    } else {
-        $('#content').css('margin-left', '0');
-        $('#sidebarCollapse').css('left', '0');
-        $('#toggleIcon').removeClass('fa-arrow-left').addClass('fa-arrow-right');
-    }
-}
-
 function setupFileInputChangeEvent() {
     document.getElementById('fileInput').addEventListener('change', function (event) {
         Array.from(event.target.files).forEach(function (file) {
